eb53a1ecec930a2aed63a9e67694d7bb5d21b7b0
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-13 14:31:00 +0100

    Remove legacyPackages assignment in flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
   outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       inherit system;
