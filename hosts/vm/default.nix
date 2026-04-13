386d7157243b437a7ad515ee3f1d50d3927c8dae
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-13 18:34:25 +0100

    Update default.nix

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -3,8 +3,9 @@
     ./hardware-configuration.nix
     ../../core
     ../../desktop-environments/plasma
-
     ../../modules/cli-essential
-
   ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
 }
