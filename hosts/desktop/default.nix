04c04724aeb735aec7d7b3a809ff80bb5081824b
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-15 21:56:26 +0100

    Update default.nix

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,4 +3,9 @@
     ./hardware-configuration.nix
     ../../profiles/desktop
   ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 30;
+  boot.loader.efi.canTouchEfiVariables = true;
+
 }
