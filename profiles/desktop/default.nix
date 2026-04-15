117eda6322fc3cdf66564830b521a56064fab176
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-15 21:59:23 +0100

    Update default.nix

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -3,6 +3,7 @@
     ../../modules/common
     ../../modules/feature-sets/cli-essentials
     ../../modules/feature-sets/gui-essentials
+    ../../modules/feature-sets/communication
     ../../modules/feature-sets/plasma
   ];
 }
