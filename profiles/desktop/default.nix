1ee2e63e37da81944ebbe5dcc5a44ab89d57fc2f
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-15 21:48:51 +0100

    Remove NVIDIA hardware module from imports

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -3,7 +3,6 @@
 {
   imports = [
     ../../modules/common
-    ../../modules/hardware/nvidia
     ../../modules/feature-sets/cli-essentials
     ../../modules/feature-sets/gui-essentials
     ../../modules/feature-sets/plasma
