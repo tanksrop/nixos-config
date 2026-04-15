2fc82ab13aa1832d3f30738c37557e7bf4460167
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-15 21:40:21 +0100

    Add default.nix for desktop profile configuration

---
+++ profiles/desktop/default.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  imports = [
+    ../../modules/common
+    ../../modules/hardware/nvidia
+    ../../modules/feature-sets/cli-essentials
+    ../../modules/feature-sets/gui-essentials
+    ../../modules/feature-sets/plasma
+  ];
+}
