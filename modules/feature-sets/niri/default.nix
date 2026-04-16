7c8aa72c78ffd7db17d8066a931e5d93ca0e44ba
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-17 00:29:24 +0100

    Update default.nix

--- modules/feature-sets/niri/default.nix
+++ modules/feature-sets/niri/default.nix
@@ -2,15 +2,35 @@
 
 let
   hmDesktop = import ./home.nix;
+
+  normalUsers =
+    lib.attrNames (
+      lib.filterAttrs (_: u: u.isNormalUser or false) config.users.users
+    );
+
+  hmUsers =
+    builtins.listToAttrs (
+      map (u: {
+        name = u;
+        value = hmDesktop;
+      }) normalUsers
+    );
 in
 {
   programs.niri.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  security.rtkit.enable = true;
+  xdg.portal.enable = true;
 
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
 
-    users = lib.mapAttrs (_: _: hmDesktop)
-      (lib.filterAttrs (_: u: u.isNormalUser or false) config.users.users);
+    users = hmUsers;
   };
 }
