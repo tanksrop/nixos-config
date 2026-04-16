e9c7074b652e5962a7548ce30f31ec2f5d7d9098
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-17 00:44:08 +0100

    Create kitty.nix

---
+++ modules/feature-sets/niri/apps/kitty.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+
+    settings = {
+      font_family = "JetBrains Mono";
+      font_size = 12;
+
+      background = "#1e1e2e";
+      foreground = "#cdd6f4";
+
+      cursor_shape = "block";
+      scrollback_lines = 10000;
+
+      enable_audio_bell = false;
+    };
+  };
+}
