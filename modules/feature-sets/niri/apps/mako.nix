678d8569e756471127c5cfb490712135786ce5ea
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-17 00:45:18 +0100

    Add configuration for Mako service

---
+++ modules/feature-sets/niri/apps/mako.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  services.mako = {
+    enable = true;
+
+    settings = {
+      default-timeout = 5000;
+
+      background-color = "#1e1e2e";
+      text-color = "#cdd6f4";
+      border-color = "#89b4fa";
+
+      border-size = 2;
+      padding = "10";
+      margin = "10";
+    };
+  };
+}
