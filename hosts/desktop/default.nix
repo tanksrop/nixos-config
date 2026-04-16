73ab1a9561fa2f0bdbbf0f56ff2bd29f34847821
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-04-17 00:17:57 +0100

    Update default.nix

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,12 +1,21 @@
+{ config, pkgs, home-manager, ... }:
+
 {
   imports = [
     ./hardware-configuration.nix
     ../../profiles/desktop
     ../../modules/hardware/nvidia
+    home-manager.nixosModules.home-manager
   ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 30;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+
+    users.yourUser = import ../../home/desktop.nix;
+  };
 }
