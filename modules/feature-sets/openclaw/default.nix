dbf8a96ae6a59d62b31e66c579f1ad6e5b17563f
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-05-08 16:39:58 +0100

    Update default.nix

--- modules/feature-sets/openclaw/default.nix
+++ modules/feature-sets/openclaw/default.nix
@@ -1,13 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  services.ollama = {
-    enable = true;
-
-    package = pkgs.ollama.overrideAttrs (old: {
-      buildInputs = old.buildInputs ++ [
-        pkgs.cudaPackages.cudatoolkit
-      ];
-    });
-  };
+services.ollama = {
+  enable = true;
+  package = pkgs.ollama-cuda;
+};
 }
