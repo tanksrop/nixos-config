e8f5999e08bafd2aa05a6160443664a919d6b48b
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-05-08 17:38:50 +0100

    Update default.nix

--- modules/feature-sets/openclaw/default.nix
+++ modules/feature-sets/openclaw/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.config.permittedInsecurePackages = [
+    "openclaw-2026.4.11"
+  ];
+
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
