d865c2423861f71c29cf64167611b60be5dfa5b5
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-05-08 16:45:46 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    openclaw.url = "github:Scout-DJ/openclaw-nix";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
