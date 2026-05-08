2efa4e5db2390a1ff0233b70f3372c37c66a75f3
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-05-08 11:26:30 +0100

    Update default.nix

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -6,7 +6,6 @@
 
     ../../modules/common
     ../../modules/feature-sets/cli-essentials
-    inputs.spicetify-nix.nixosModules.spicetify
     ../../modules/feature-sets/gui-essentials
     ../../modules/feature-sets/communication
     ../../modules/feature-sets/plasma
