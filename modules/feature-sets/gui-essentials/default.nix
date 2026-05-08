4e0dd993b5cf2a69d84e1ab13b832b09b7b2adb4
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-05-08 11:18:34 +0100

    Update default.nix

--- modules/feature-sets/gui-essentials/default.nix
+++ modules/feature-sets/gui-essentials/default.nix
@@ -1,7 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
-{
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in {
   environment.systemPackages = with pkgs; [
     firefox
   ];
+
+  programs.spicetify = {
+    enable = true;
+
+    enabledExtensions = with spicePkgs.extensions; [
+      fullAppDisplay
+      popupLyrics
+    ];
+  };
 }
