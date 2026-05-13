244631b81a26331bc24b7859e791348d21edaa54
tanksrop <76452125+tanksrop@users.noreply.github.com>
2026-05-13 19:10:37 +0100

    Update default.nix

--- modules/feature-sets/gui-essentials/default.nix
+++ modules/feature-sets/gui-essentials/default.nix
@@ -1,17 +1,19 @@
 { config, pkgs, inputs, ... }:
 
 {
+  # Import Spicetify module
   imports = [
     inputs.spicetify-nix.nixosModules.default
   ];
 
+  # System packages
   environment.systemPackages = [
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     pkgs.spotify
   ];
 
+  # Enable Spicetify
   services.spicetify.enable = true;
-
   services.spicetify.theme = "Catppuccin";
   services.spicetify.enableExtensions = true;
 }
